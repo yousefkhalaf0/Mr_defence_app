fa1d974d00764dcb4b913b5b41290ae6b2d8752c
yousefkhalaf0 <yousefkhalaf667@gmail.com>
2025-04-10 17:14:14 +0200

    feat(onboarding): reintroduce OnBoardingCubit and state management with updated model

---
+++ lib/features/on_boarding/data/models/on_boarding_model.dart
@@ -0,0 +1,36 @@
+import 'package:app/core/utils/assets.dart';
+
+class OnBoardingModel {
+  String? image;
+  String? title;
+  String? description;
+
+  OnBoardingModel({this.image, this.title, this.description});
+
+  static List<OnBoardingModel> data = [
+    OnBoardingModel(
+      image: AssetsData.onBoardingImage1,
+      title: 'Your safety is Our Priority!',
+      description:
+          'Stay informed and protected with real-time crime alerts, emergency tools, and community support all in one app.',
+    ),
+    OnBoardingModel(
+      image: AssetsData.onBoardingImage2,
+      title: 'Real-Time Alerts at Your Fingertips',
+      description:
+          'Stay updated on local crimes, fires, and accidents. Report incidents with photos or videos to help keep your community safe.',
+    ),
+    OnBoardingModel(
+      image: AssetsData.onBoardingImage3,
+      title: 'Emergency Help, One Tap Away',
+      description:
+          'Use the SOS button to instantly notify emergency contacts and authorities. Share your location with trusted circles and set up safe zones for added peace of mind.',
+    ),
+    OnBoardingModel(
+      image: AssetsData.onBoardingImage4,
+      title: 'Together, We build a safer Community',
+      description:
+          'Join a network of users who verify incidents and help filter out false reports. Our AI ensures you get accurate, reliable alerts.',
+    ),
+  ];
+}
